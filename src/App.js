32e3c2f1b957756e882e31950325f66020446491
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-10-14 12:56:46 +0300

    edited App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          React-Travis deploy !
         </a>
       </header>
     </div>
